393c39204adbc5153cc0d28db2e2563312fb2fb9
Fiona Margrie <fiona@pmafilmtv.com>
2023-12-23 13:21:32 +0000

    modified:   receipt-printer.py

--- receipt-printer.py
+++ receipt-printer.py
@@ -1,94 +1,15 @@
 from escpos.printer import Serial, Dummy
 
-import PySimpleGUI as sg
-from PIL import Image, ImageTk
-
-
-def print_values(data):
-    print("Printing values:")
-    for entry in data:
-        print(f"Text: {entry['text']}, Price: {entry['price']}")
-
-
-def main():
-    sg.theme("Default1")
-
-    # Define layout
-    layout = [
-        [sg.Text(text="This is a very basic PySimpleGUI layout")],
-        [sg.InputText(), sg.Button(button_text="Browse...")],
-        [sg.Image()],
-        [sg.Text(text="Item"), sg.InputText(), sg.Text(text="Price"), sg.InputText()],
-        [
-            sg.Button(button_text="Print", key="-ExampleKey-"),
-            sg.Button(button_text="Exit"),
-            sg.Button(button_text="Add item"),
-        ],
-    ]
-
-    # Create window
-    window = sg.Window("Image and Data Entry", layout, resizable=True, finalize=True)
-
-    while True:
-        event, values = window.read()
-
-        if event == sg.WINDOW_CLOSED:
-            break
-
-        # Update image preview when a file is selected
-        if event == "-FILE-":
-            file_path = values["-FILE-"]
-            try:
-                image = Image.open(file_path)
-                image.thumbnail((400, 400))
-                photo = ImageTk.PhotoImage(image)
-                window["-IMAGE-"].update(data=photo)
-            except Exception as e:
-                print(f"Error loading image: {e}")
-
-        # Add and remove additional sets
-        if event == "Add item":
-            layout_extension = [
-                [
-                    sg.Text("Text"),
-                    sg.InputText(key="-TEXT2-", size=(20, 1)),
-                    sg.Text("Price"),
-                    sg.InputText(key="-PRICE2-", size=(10, 1)),
-                ],
-            ]
-            window.extend_layout(window, layout_extension)
-
-        # Print values
-        if event == "Print":
-            data = [
-                {"text": values["-TEXT-"], "price": values["-PRICE-"]},
-                {
-                    "text": values.get("-TEXT2-", ""),
-                    "price": values.get("-PRICE2-", ""),
-                },
-                {
-                    "text": values.get("-TEXT3-", ""),
-                    "price": values.get("-PRICE3-", ""),
-                },
-            ]
-            print_values(data)
-
-    window.close()
-
-
-if __name__ == "__main__":
-    main()
-
-
-# p = Serial(
-#     devfile="COM5",
-#     baudrate=38400,
-#     bytesize=8,
-#     parity="N",
-#     stopbits=1,
-#     timeout=1.00,
-#     dsrdtr=True,
-# )
+p = Serial(
+    # devfile="COM5",
+    devfile="/dev/tty.usbserial-8340"
+    baudrate=38400,
+    bytesize=8,
+    parity="N",
+    stopbits=1,
+    timeout=1.00,
+    dsrdtr=True,
+)
 
 d = Dummy(profile="TM-H6000III")
 
